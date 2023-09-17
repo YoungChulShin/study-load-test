bdcd73c8ac9a2f3fbf5a7e8250fdd70ecf87e64b
YoungChulShin <go1323@gmail.com>
2023-09-17 10:22:02 +0900

    01.basic-test: 로그 및 설정 값 추가

--- 01.basic-load-test/test-server/test-server/src/main/kotlin/study/jmeter/basic/testserver/controller/TestController.kt
+++ 01.basic-load-test/test-server/test-server/src/main/kotlin/study/jmeter/basic/testserver/controller/TestController.kt
@@ -7,17 +7,21 @@ import kotlin.random.Random
 @RestController
 class TestController {
 
-    @PostMapping("api/v1/test/max-500-millis")
+    @PostMapping("/test/local/random/max-500-millis")
     fun testMax500Millis(): String {
         val delay = Random.nextLong(50, 500)
         Thread.sleep(delay)
-        return "OK - ${Thread.currentThread().name}"
+        val message = "OK - $delay - ${Thread.currentThread().name}"
+        println(message)
+        return message
     }
 
-    @PostMapping("api/v1/test/max-1000-millis")
+    @PostMapping("/test/local/random/max-1000-millis")
     fun testMax1Seconds(): String {
         val delay = Random.nextLong(50, 1000)
         Thread.sleep(delay)
-        return "OK - ${Thread.currentThread().name}"
+        val message = "OK - $delay - ${Thread.currentThread().name}"
+        println(message)
+        return message
     }
 }
